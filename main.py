7d63cb5a30c8fb7d0186e3e4a37accd5f2165408
MaksymP <maximus-ms@ukr.net>
2024-03-09 15:57:42 +0200

    Small fixes

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ heapq.heapify(cables)
 cost = 0
 
 print(f"All cables ({len(cables)} parts): {cables} ")
-print(f"We expect to get a cable with length: {sum(cables)}")
+print(f"We expect to get a cable length: {sum(cables)}")
 
 for _ in range(len(cables) - 1):
     new_cable = heapq.heappop(cables) + heapq.heappop(cables)
     heapq.heappush(cables, new_cable)
     cost += new_cable
 
-print(f"We got a cable with length: {cables[0]}")
+print(f"We got a cable length: {cables[0]}")
 print(f"Total cost: {cost}")
